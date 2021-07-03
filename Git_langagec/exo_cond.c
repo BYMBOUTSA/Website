a3641292614bb877b8a737baa803e5f0e837303c
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-03 12:52:56 +0200

    first commit

--- Git_langagec/exo_cond.c
+++ Git_langagec/exo_cond.c
@@ -2,6 +2,19 @@
 
 int main(void) {
 
-    printf("Hello world !\n");
+    //déclaration de nos variables en types entières
+    int som_achats = 0, som_donnee = 0, monnaie = 0;
+    int bill500 = 0, bill200 = 0, bill100 = 0, bill50 = 0, bill20 = 0, bill10 = 0, bill5 = 0, coin2 = 0, coin1 = 0;
+
+    //Demande à l'utilisateur de saisir le montant total des achats et la somme donnée
+    printf("Entrez le montant total des achats : ");
+    scanf("%d", &som_achats);
+    printf("Entrez la somme donnée : ");
+    scanf("%d", &som_donnee);
+
+    
+
+
+
     return 0;
 }
