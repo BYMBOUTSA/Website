23916b5a5a4614f570256ce5b4d2e49188382314
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-09 09:49:48 +0200

    Boucle while

---
+++ Git_langagec/boucles_v2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+   int i = 0;
+
+   while(i < 10) {
+       printf("i vaut %d\n", i);
+       i++;
+   }
+
+   printf("Boucle terminée !\n");
+
+    return 0;
+}
