9af132d474d2f89b05014d0a579278903ac2c192
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-09 08:12:22 +0200

    Première boucle

--- Git_langagec/boucles_v1.c
+++ Git_langagec/boucles_v1.c
@@ -4,23 +4,12 @@ int main(void) {
 
     int i = 0;
 
-    /* while(i < 5) {
+    while(i < 5) {
         printf("Charly, tu as vu ?\n");
         i++;
-    } */
-
-    /*
-        diffférence entre la boucle while et celle do while:
-        - while fait le test des le départ et si celui-ci est faux tu n'obtient rien.
-        - do while s'éxécute au moins une fois et après tu es testé.
-    */
-    do {
-        printf("Charly, tu as vu ?\n");
-        i++;
-
     }
-    while(i < 5);
 
+    
 
     return 0;
 }
