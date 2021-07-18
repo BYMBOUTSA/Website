681921eed36a4e80125af788be83ee8ffedfa6cf
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-18 19:16:47 +0200

    Ajout de la condition  elif

--- Git_Python/cours_cond_v1.py
+++ Git_Python/cours_cond_v1.py
@@ -7,6 +7,10 @@ cond =  (not warn and system_load >= 10) #si il n'y a pas de warning et que le s
 if cond: 
     print("WARNING: Perte de ressource") #alors on a ce message
     warn = True
- 
+    
+elif not warn and system_load < 10:
+    print("Pas de soucis")
+
+'''  
 else:
-    print("Tout va bien")   
+    print("Tout va bien")  '''  
