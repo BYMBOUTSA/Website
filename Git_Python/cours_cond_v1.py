e2b13f6d1a0bb028e842cc02f7a24129875c8b7b
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-18 18:55:22 +0200

    Condition if

--- Git_Python/cours_cond_v1.py
+++ Git_Python/cours_cond_v1.py
@@ -2,8 +2,9 @@
 #       suite_si_vrai
 
 warn = False
-system_load = 5
-if not warn and system_load >= 10: #si il n'y a pas de warning et que le système load est supérieur ou égal à 10
+system_load = 25
+cond =  (not warn and system_load >= 10) #si il n'y a pas de warning et que le système load est supérieur ou égal à 10
+if cond: 
     print("WARNING: Perte de ressource") #alors on a ce message
     warn = True
     
