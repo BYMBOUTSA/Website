a3def7db3b8ec8f1355c8045b16b3034d4ff82f1
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-18 19:02:20 +0200

    Ajout boucle else

--- Git_Python/cours_cond_v1.py
+++ Git_Python/cours_cond_v1.py
@@ -2,10 +2,11 @@
 #       suite_si_vrai
 
 warn = False
-system_load = 25
+system_load = 5
 cond =  (not warn and system_load >= 10) #si il n'y a pas de warning et que le système load est supérieur ou égal à 10
 if cond: 
     print("WARNING: Perte de ressource") #alors on a ce message
     warn = True
-    
-    
+ 
+else:
+    print("Tout va bien")   
