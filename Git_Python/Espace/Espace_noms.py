4b6c004e2a65faaf1b0f201f672ee6d320018539
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-07-31 11:59:44 +0200

    git global

--- Git_Python/Espace/Espace_noms.py
+++ Git_Python/Espace/Espace_noms.py
@@ -1,4 +1,4 @@
-"pour importer des modules si on veut chercher un fichier qui se nomme module.py"
+"pour importer un module si on veut chercher un fichier qui se nomme module.py"
 import sys
 sys.path
 
@@ -14,5 +14,39 @@ from random import (
 )
 
 "importation étendue avec as"
-from string import ascii_lowercase as alphabet
-alphabet_uppercase = alphabet.uppercase()
+"""from string import ascii_lowercase as alphabet
+alphabet_uppercase = alphabet.uppercase()"""
+
+"Espace de noms des modules"
+import math
+pi = math.pi
+
+"Porté des variables"
+var = 5
+def func():
+    var = 7
+func()
+
+"si on veut avoir var = 7 à la fin"
+var = 5
+def func(v):
+    v = 7
+    return v
+var = func(var)
+
+"autre manière de faire"
+var = 5
+def func():
+    global var
+    var = 7
+
+"Packages"
+'un repertoire(ex.test)'
+'Un fichier'
+'__init__.py'
+'Des fichiers python' 
+type.py
+'un fichier(ou module) nommé'
+'mod.py'
+'from package.mod import function'
+
