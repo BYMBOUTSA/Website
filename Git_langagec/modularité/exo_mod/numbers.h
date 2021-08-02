b4ef329ba33d782e1a359c17a9755acfbc33ec68
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-08-02 22:30:49 +0200

    Exo de calculatrice en utilisant la modularité

---
+++ Git_langagec/modularité/exo_mod/numbers.h
@@ -0,0 +1,15 @@
+#if !defined(__NUMBERS__H__)
+#define __NUMBERS__H__
+
+/*Prototypes*/
+void app_calc(void);
+void show_menu(void); //Pour afficher le menu principal
+void do_operation(int choice); 
+int addition(int a, int b);
+int soustraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
+
+
+
+#endif
