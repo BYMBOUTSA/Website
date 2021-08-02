f117318204bb5f4367bc1e22cd5c650e0aea04c1
BYMBOUTSA <charlymoundounga3@gmail.com>
2021-08-02 22:39:48 +0200

    Exo de calculatrice en utilisant la modularité

---
+++ Git_langagec/modularité/exo_mod/README.md
@@ -0,0 +1,15 @@
+# EXO MOD
+/*
+EXERCICE C #4
+[Révision : jusqu'à la programmation modulaire]
+> Ecrire un programme de calculatrice simple de deux nombres :
+    - afficher un menu principal proposant les 4 types de calcul (+, -, * et /) ou de quitter.
+    - une fois le calcul choisi, demander la saisie de 2 nombres et afficher le résultat.
+    - rediriger sur le menu principal.
+
+> Indications :
+    - utiliser au maximum des fonctions (pour optimiser votre code et éviter les répétitions).
+    - vérifier que la saisie au menu est correct, sinon redemander une saisie.
+    - Vérifier que les nombres saisis sont compris entre -1000 et 1000.
+    - compiler avec la commande -> gcc *.c -o prog (donnerra un exécutable prog ou prog.exe)
+*/
--- Git_langagec/modularité/exo_mod/main.c
+++ Git_langagec/modularité/exo_mod/main.c
@@ -1,19 +1,3 @@
-/*
-EXERCICE C #4
-[Révision : jusqu'à la programmation modulaire]
-> Ecrire un programme de calculatrice simple de deux nombres :
-    - afficher un menu principal proposant les 4 types de calcul (+, -, * et /) ou de quitter.
-    - une fois le calcul choisi, demander la saisie de 2 nombres et afficher le résultat.
-    - rediriger sur le menu principal.
-
-> Indications :
-    - utiliser au maximum des fonctions (pour optimiser votre code et éviter les répétitions).
-    - vérifier que la saisie au menu est correct, sinon redemander une saisie.
-    - Vérifier que les nombres saisis sont compris entre -1000 et 1000.
-    - compiler avec la commande -> gcc *.c -o prog (donnerra un exécutable prog ou prog.exe)
-*/
-
-
 #include <stdio.h>
 #include "numbers.h"
 
@@ -22,6 +6,6 @@ int main(void) {
     
     app_calc();
     
-
+//----------------------------------------------------------------
     return 0;
 }
